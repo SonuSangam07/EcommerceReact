20d6f3fb051de9e0bf095af401d43bef1241656b
SonuSangam7 <sonusangamcsk1@gmail.com>
2023-02-01 17:01:23 +0530

    Contact page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import CartProvider from "./components/store/CartProvider";
 import FrontPage from "./components/FrontPage/FinalPage";
 import Cart from "./components/Cart/Cart";
+import {Route} from 'react-router-dom';
+import Header from "./components/FrontPage/header";
+import ContactUs from "../src/components/pages/ContactUs";
+import Footer from "./components/FrontPage/footer";
+
+import About from "./components/pages/About";
+
+import Home from "./components/pages/Home"
 function App() {
   const [CartisShown, setCartShow] = useState(false);
 
@@ -12,12 +20,27 @@ function App() {
   const cartRemoveHandler = () => {
     setCartShow(false);
   };
-    
+  const contactedUserDetails = (userdata) => {
+    console.log(userdata)
+  }
     
   return (
     <CartProvider>
-     <FrontPage cartFunc={cartShowHandler}/>
-      {CartisShown && <Cart cartFunc={cartRemoveHandler}/>}
+         <Header cartFunc={cartShowHandler} />
+      <Route path='/store'>
+        <FrontPage />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/home'>
+        <Home/>
+      </Route>
+      <Route path='/contact'>
+        <ContactUs contactDetails={contactedUserDetails}/>
+      </Route>
+    {CartisShown && <Cart cartFunc={cartRemoveHandler} />}
+      <Footer />
       </CartProvider>
   );
 }
